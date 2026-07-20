5e5ada89803457563259b1ff98acb57871146c58
bhimeswar101 <bhimeswarbhimeswar@gmail.com>
2026-07-20 23:43:16 +0530

    Use Render backend URL 2

--- mobile/src/api/api.ts
+++ mobile/src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "../storage/authStorage";
 
 const api = axios.create({
-  baseURL: "https://paid-media-locker-rl7v.onrender.com/api",
+  baseURL: "https://paid-media-locker-vy2t.onrender.com/api",
 });
 
 api.interceptors.request.use(async (config) => {
