bc5b634589b19e21a0518af503439f042e7b32b7
bhimeswar101 <bhimeswarbhimeswar@gmail.com>
2026-07-20 18:44:11 +0530

    Use Render backend

--- mobile/src/api/api.ts
+++ mobile/src/api/api.ts
@@ -2,29 +2,16 @@ import axios from "axios";
 import { getToken } from "../storage/authStorage";
 
 const api = axios.create({
-  baseURL: "http://192.168.31.102:5000/api",
+  baseURL: "https://paid-media-locker-rl7v.onrender.com/api",
 });
 
 api.interceptors.request.use(async (config) => {
   const token = await getToken();
 
-  console.log("=================================");
-  console.log("INTERCEPTOR TOKEN:", token);
-
   if (token) {
     config.headers = config.headers ?? {};
     config.headers.Authorization = `Bearer ${token}`;
-
-    console.log(
-      "AUTH HEADER:",
-      config.headers.Authorization
-    );
-  } else {
-    console.log("NO TOKEN FOUND");
   }
-
-  console.log("REQUEST:", config.url);
-  console.log("=================================");
 
   return config;
 });
